d0ab540612825b73524e4d18c8c76c9ceb81a6d4
Azeez Abdulsalam <azeezade04@gmail.com>
2024-03-03 23:39:45 +0100

    updated package.json

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,8 @@ import 'dotenv/config';
 import ExpressApp from './utility/ExpressApp';
 import connectDB from './config/db';
 import dotenv from 'dotenv';
-import * as fs from 'fs';
-import http from 'http';
-import https from 'https';
 
 dotenv.config();
-
-// const privateKey = process.env.NODE_ENV === 'production' ? fs.readFileSync('../privkey.pem', 'utf8') : '';
-// // const privateKey = process.env.NODE_ENV === 'production' ? fs.readFileSync('/etc/letsencrypt/live/api.gettrill.com/privkey.pem', 'utf8') : '';
-
-// const certificate = process.env.NODE_ENV === 'production' ? fs.readFileSync('/etc/letsencrypt/live/api.gettrill.com/fullchain.pem', 'utf8') : '';
 
 const StartServer = async () => {
     const app = express();
@@ -23,18 +15,6 @@ const StartServer = async () => {
     await connectDB();
 
     const PORT = process.env.PORT || 1335;
-
-    // Starting either http or https server
-    // const server =
-    //     process.env.NODE_ENV === 'production'
-    //         ? https.createServer(
-    //               {
-    //                   key: privateKey,
-    //                   cert: certificate,
-    //               },
-    //               app,
-    //           )
-    //         : http.createServer(app);
 
     app.listen(PORT, () => {
         log.info(`Server listening on: http://localhost:${PORT}`);
